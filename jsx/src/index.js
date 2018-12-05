fa2fce61c94aaf54d49b087ad778f0a7f4274716
Istvan Acs <istvan.acs89@gmail.com>
2018-12-05 16:04:45 +0000

    Add more Js variable to referencing in JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,21 +3,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-function getButtonText() {
-    return 'Click on me!'
-}
+// function getButtonText() {
+//     return 'Click on me!'
+// }
+
 // Create a react component
 const App = () => {
-    // const buttonText = 'Click me!'
+    const buttonText = 'Click me!'
+    const labelText = 'Enter name: '
+
     return (
         <div>
             <label className="label" htmlFor="name">
-                Enter Name:
+                {labelText}
             </label>
             <input id="name" type="text" />
             <button style={{ backgroundColor: "blue", color: "white" }}>
-                {/* {buttonText} */}
-                {getButtonText()}
+                {buttonText}
             </button>
         </div>
     )

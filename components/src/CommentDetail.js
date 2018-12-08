223c9ffae2d773b50f3020aaa178560b6e4b528e
Istvan Acs <istvan.acs89@gmail.com>
2018-12-08 12:17:48 +0000

    Destructuring props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const CommentDetail = props => {
+const CommentDetail = ({ avatar, author, timeAgo, content }) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={props.avatar}/>
+        <img alt="avatar" src={avatar}/>
       </a>
       <div className="content">
         <a href="/" className="author">
-          {props.author}
+          {author}
         </a>
         <div className="metadata">
-          <span className="date">{props.timeAgo}</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">{props.content}</div>
+        <div className="text">{content}</div>
       </div>
     </div>
   )

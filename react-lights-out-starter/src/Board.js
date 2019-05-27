070534077be84040e830a2c25a1a9d0880918f0a
Istvan Acs <istvan.acs89@gmail.com>
2019-05-27 18:29:50 +0100

    Styling the game

--- react-lights-out-starter/src/Board.js
+++ react-lights-out-starter/src/Board.js
@@ -99,7 +99,14 @@ class Board extends Component {
 
     // if the game is won, just show a winning msg & render nothing else
     if(this.state.hasWon) {
-      return <h1>YOU WON!!!</h1>
+      return (
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!!</span>
+          </div>
+        </div>
+      )
     }
     // TODO
 
@@ -116,9 +123,15 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
-      <table className="Board">
-        <tbody>{tblBoard}</tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
     )
   }
 }

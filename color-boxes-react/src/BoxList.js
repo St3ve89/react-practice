95da74c975322c78b7cc539a140776f6707b7359
Istvan Acs <istvan.acs89@gmail.com>
2019-06-01 12:06:31 +0100

    Creating the boxform component

--- color-boxes-react/src/BoxList.js
+++ color-boxes-react/src/BoxList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
 
 class BoxList extends Component {
   constructor(props) {
     super(props);
     this.state = {boxes: [{width:10, height: 40, color: "orange"}]};
+    this.create = this.create.bind(this);
   }
+
+  create(newBox) {
+    this.setState({
+      boxes: [...this.state.boxes, newBox]
+    })
+  }
+
   render() {
     const boxes = this.state.boxes.map(box => (
       <Box 
@@ -16,7 +25,8 @@ class BoxList extends Component {
     ))
     return (
       <div>
-        <hi>Color Box Maker Thingy</hi>
+        <h1>Color Box Maker Thingy</h1>
+        <NewBoxForm createBox={this.create} />
         {boxes}
       </div>
     )
---
+++ color-boxes-react/src/NewBoxForm.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+class NewBoxForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: '',
+      width: '',
+      color: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.createBox(this.state);
+    this.setState({
+      height: '',
+      width: '',
+      color: ''
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="height">Height</label>
+          <input 
+            type="text"
+            name="height"
+            value={this.state.height}
+            onChange={this.handleChange}
+            id="height"/>
+        </div>
+        <div>
+          <label htmlFor="width">Width</label>
+          <input 
+            type="text"
+            name="width"
+            value={this.state.width}
+            onChange={this.handleChange}
+            id="width"/>
+        </div>
+        <div>
+          <label htmlFor="color">Color</label>
+          <input 
+            type="text"
+            name="color"
+            value={this.state.color}
+            onChange={this.handleChange}
+            id="color"/>
+        </div>
+        <button>Add New Box</button>
+      </form>
+    )
+  }
+}
+
+
+export default NewBoxForm;
